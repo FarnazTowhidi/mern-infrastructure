23aef3c59022f1e012509fdd7358c48def27d78f
farnaz <farnaz.towhidi@gmail.com>
2022-11-25 12:18:36 -0500

    MERN Infrastructure Part 1

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,31 @@
 
 import "./App.css";
+import { useState } from "react";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import AuthPage from "../AuthPage/AuthPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import { Routes,Route } from 'react-router-dom';
+import NavBar from "../../components/NavBar/NavBar";
+
 
 function App() {
+  const [user,setUser] = useState({})
+
   return (
-    <main className="App">App</main>
+    <main className="App">
+       <NavBar /> 
+      { user ? (
+      <>
+      
+      <Routes>
+        <Route path="/orders/new" element={<NewOrderPage />} />
+        <Route path="/orders/" element={<OrderHistoryPage />} />
+      </Routes>
+      </>
+       ): (
+       <AuthPage />)}
+    </main>
+
   );
 }
 
