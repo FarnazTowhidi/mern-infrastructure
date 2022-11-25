18d86d7f36560cf8fd4c6fc13cd5ab074f4789fe
farnaz <farnaz.towhidi@gmail.com>
2022-11-25 15:41:25 -0500

    Update mern infrustructure 3

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import SignUpForm from '../../components/SignUpForm/SignUpForm'
 
 function AuthPage() {
   return (
-    <h1>AuthPage</h1>
+    <>
+      <h1>AuthPage</h1>
+      <SignUpForm />
+    </>
   )
 }
 
